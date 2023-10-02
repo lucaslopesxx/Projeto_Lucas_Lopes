386994833124fee907f37e9f266c45a0c872de61
Lucas Lopes <104603745+lucaslopesxx@users.noreply.github.com>
2023-10-01 21:36:06 -0300

    Add files via upload

---
+++ classeA.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+private:
+    int A1;
+    float A2;
+
+public:
+    // Construtor
+    A(int a1, float a2) : A1(a1), A2(a2) {}
+
+    // Getters e Setters
+    int getA1() const
+    {
+        return A1;
+    }
+
+    void setA1(int a1)
+    {
+        A1 = a1;
+    }
+
+    float getA2() const
+    {
+        return A2;
+    }
+
+    void setA2(float a2)
+    {
+        A2 = a2;
+    }
+
+    // Métodos MA1 e MA2
+    void MA1()
+    {
+        cout << "MA1" << endl;
+    }
+
+    void MA2()
+    {
+        cout << "MA2" << endl;
+    }
+};
---
+++ classeB.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class B
+{
+private:
+    int B1;
+    float B2;
+
+public:
+    // Construtor
+    B(int b1, float b2) : B1(b1), B2(b2) {}
+
+    // Getters e Setters
+    int getB1() const
+    {
+        return B1;
+    }
+
+    void setB1(int b1)
+    {
+        B1 = b1;
+    }
+
+    float getB2() const
+    {
+        return B2;
+    }
+
+    void setB2(float b2)
+    {
+        B2 = b2;
+    }
+
+    // Métodos MB1 e MB2
+    void MB1()
+    {
+        cout << "MB1" << endl;
+    }
+
+    void MB2()
+    {
+        cout << "MB2" << endl;
+    }
+};
