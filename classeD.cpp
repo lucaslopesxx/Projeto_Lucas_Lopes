570bed4907b50bf5995a0db13436aa62c5ed3ca8
lucaslopesxx <lucasalvarengalopes@gmail.com>
2023-10-01 22:27:33 -0300

    Criação da Classe D

---
+++ classeD.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class D
+{
+private:
+    int D1;
+    char D2;
+
+public:
+    // Construtor
+    D(int d1, char d2) : D1(d1), D2(d2) {}
+
+    // Getters e Setters
+    int getD1() const
+    {
+        return D1;
+    }
+
+    void setD1(int d1)
+    {
+        D1 = d1;
+    }
+
+    char getD2() const
+    {
+        return D2;
+    }
+
+    void setD2(char d2)
+    {
+        D2 = d2;
+    }
+
+    // Métodos MD1 e MD2
+    void MD1()
+    {
+        cout << "MD1" << endl;
+    }
+
+    void MD2()
+    {
+        cout << "MD2" << endl;
+    }
+};
